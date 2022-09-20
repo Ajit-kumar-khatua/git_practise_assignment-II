764b10849ff3d40d954d2809b1ea487b26e93089
unknown <ajitkumarkhatua286@gmail.com>
2022-09-20 11:43:21 +0530

    update the correct code

--- prime.js
+++ prime.js
@@ -1,10 +1,19 @@
-function prime(num){
-	for(let i=0;i<=num;i++){
-		if(i%num){
-			return i;
+function prime(number){
+	let factor=0
+	for(let i=0;i<=number;i++){
+		if(i%number){
+			factor++;
 		}
 	}
+	if(factor==2){
+		return true;
+	}
+	return false;
 }
 
-let x=prime(19)
-console.log(x);
+let answer=prime(19)
+ if(answer==true){
+ 	console.log("Prime Number");
+ }else{
+ 	console.log("Not a Prime Number")
+ }
